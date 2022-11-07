e743aad20ae7b12a8df5ec95dabef5d817eab1c4
link2xt <link2xt@testrun.org>
2022-11-07 20:55:08 +0000

    async fixture

--- deltachat-rpc-client/tests/test_something.py
+++ deltachat-rpc-client/tests/test_something.py
@@ -1,12 +1,15 @@
 import pytest
+import pytest_asyncio
 
 import deltachat_rpc_client
 
-@pytest.mark.asyncio
-async def test_system_info():
-    rpc_server = await deltachat_rpc_client.start_rpc_server()
-    assert rpc_server
+@pytest_asyncio.fixture
+async def rpc_server():
+    return await deltachat_rpc_client.start_rpc_server()
 
+
+@pytest.mark.asyncio
+async def test_system_info(rpc_server):
     system_info = await rpc_server.get_system_info()
     assert "arch" in system_info
     assert "deltachat_core_version" in system_info
