dd11c1ff12cf7fbc74aa75b272c31eed275e9b91
link2xt <link2xt@testrun.org>
2022-11-07 21:04:56 +0000

    python: test email address validity

--- deltachat-rpc-client/tests/test_something.py
+++ deltachat-rpc-client/tests/test_something.py
@@ -3,6 +3,7 @@ import pytest_asyncio
 
 import deltachat_rpc_client
 
+
 @pytest_asyncio.fixture
 async def rpc_server():
     return await deltachat_rpc_client.start_rpc_server()
@@ -13,3 +14,17 @@ async def test_system_info(rpc_server):
     system_info = await rpc_server.get_system_info()
     assert "arch" in system_info
     assert "deltachat_core_version" in system_info
+
+
+@pytest.mark.asyncio
+async def test_email_address_validity(rpc_server):
+    valid_addresses = [
+        "email@example.com",
+        "36aa165ae3406424e0c61af17700f397cad3fe8ab83d682d0bddf3338a5dd52e@yggmail@yggmail",
+    ]
+    invalid_addresses = ["email@", "example.com", "emai221"]
+
+    for addr in valid_addresses:
+        assert await rpc_server.check_email_validity(addr)
+    for addr in invalid_addresses:
+        assert not await rpc_server.check_email_validity(addr)
