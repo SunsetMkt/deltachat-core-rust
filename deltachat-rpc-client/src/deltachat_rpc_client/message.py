00395cd65571b7f6eae80da7321798ab77f20954
link2xt <link2xt@testrun.org>
2022-11-16 22:38:41 +0000

    fix for old python

--- deltachat-rpc-client/src/deltachat_rpc_client/message.py
+++ deltachat-rpc-client/src/deltachat_rpc_client/message.py
@@ -4,6 +4,6 @@ class Message:
         self.account_id = account_id
         self.msg_id = msg_id
 
-    async def send_reaction(self, reactions: list[str]):
+    async def send_reaction(self, reactions):
         msg_id = await self.rpc.send_reaction(self.account_id, self.msg_id, reactions)
         return Message(self.rpc, self.account_id, msg_id)
